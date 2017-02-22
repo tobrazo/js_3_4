8ffd876818aa092a5e2b5753815dfb788dcd8472
tobrazo <tobrazo@gmail.com>
2017-02-22 11:50:27 +0200

    2 commit

--- home3_4.js
+++ home3_4.js
@@ -3,61 +3,62 @@ var header = document.createElement('div');
 
 header.className = "headerText";
 
-header.innerHTML = "<h2 style=\"text-align: center;\">Тест по программированию</h2>";
+header.innerHTML = "<h2>Тест по программированию</h2>";
 
 document.body.insertBefore(header, document.body.firstChild);
 
 
-
 	var query1 = document.createElement('div');
 
-	query1.className = "query1Text";
+	query1.className = "queryText";
 
-	query1.innerHTML = "<p style=\"padding-left: 350px; margin-bottom: 5px;\">1. Вопрос №1</p>";
+	query1.innerHTML = "<p>1. Вопрос №1</p>";
 
 	document.body.appendChild(query1);
 
 	var checkFirst = document.createElement('div');
 
-	checkFirst.className = "checkBoxFirst";
+	checkFirst.className = "checkBox";
 
-	checkFirst.innerHTML = "<p style=\"padding-left: 370px; margin-top: 5px;\"><input type=\"checkbox\" name=\"\" value=\"\">Вариант ответа №1<br><input type=\"checkbox\" name=\"\" value=\"\">Вариант ответа №2<br><input type=\"checkbox\" name=\"\" value=\"\">Вариант ответа №3<br></p>";
+	checkFirst.innerHTML = "<p><input type=\"checkbox\" name=\"\" value=\"\">Вариант ответа №1<br><input type=\"checkbox\" name=\"\" value=\"\">Вариант ответа №2<br><input type=\"checkbox\" name=\"\" value=\"\">Вариант ответа №3<br></p>";
 
 	document.body.appendChild(checkFirst);
 
 
-
 	var query2 = document.createElement('div');
 
-	query2.className = "query2Text";
+	query2.className = "queryText";
 
-	query2.innerHTML = "<p style=\"padding-left: 350px; margin-bottom: 5px;\">2. Вопрос №2</p>";
+	query2.innerHTML = "<p>2. Вопрос №2</p>";
 
 	document.body.appendChild(query2);
 
 	var checkSec = document.createElement('div');
 
-	checkSec.className = "checkBoxSec";
+	checkSec.className = "checkBox";
 
-	checkSec.innerHTML = "<p style=\"padding-left: 370px; margin-top: 5px;\"><input type=\"checkbox\" name=\"\" value=\"\">Вариант ответа №1<br><input type=\"checkbox\" name=\"\" value=\"\">Вариант ответа №2<br><input type=\"checkbox\" name=\"\" value=\"\">Вариант ответа №3<br></p>";
+	checkSec.innerHTML = "<p><input type=\"checkbox\" name=\"\" value=\"\">Вариант ответа №1<br><input type=\"checkbox\" name=\"\" value=\"\">Вариант ответа №2<br><input type=\"checkbox\" name=\"\" value=\"\">Вариант ответа №3<br></p>";
+
 
 	document.body.appendChild(checkSec);
 
 
-
 	var query3 = document.createElement('div');
 
-	query3.className = "query3Text";
+	query3.className = "queryText";
 
-	query3.innerHTML = "<p style=\"padding-left: 350px; margin-bottom: 5px;\">3. Вопрос №3</p>";
+	query3.innerHTML = "<p>3. Вопрос №3</p>";
+
 
 	document.body.appendChild(query3);
 
 	var checkThird = document.createElement('div');
 
-	checkThird.className = "checkBoxThird";
+	checkThird.className = "checkBox";
 
-	checkThird.innerHTML = "<p style=\"padding-left: 370px; margin-top: 5px;\"><input type=\"checkbox\" name=\"\" value=\"\">Вариант ответа №1<br><input type=\"checkbox\" name=\"\" value=\"\">Вариант ответа №2<br><input type=\"checkbox\" name=\"\" value=\"\">Вариант ответа №3<br></p>";
+	
+	checkThird.innerHTML = "<p><input type=\"checkbox\" name=\"\" value=\"\">Вариант ответа №1<br><input type=\"checkbox\" name=\"\" value=\"\">Вариант ответа №2<br><input type=\"checkbox\" name=\"\" value=\"\">Вариант ответа №3<br></p>";
+
 
 	document.body.appendChild(checkThird);
 
@@ -66,5 +67,7 @@ document.body.insertBefore(header, document.body.firstChild);
 
 		button.className = "buttonResult";
 
-		button.innerHTML = "<p style=\"text-align: center; \"><input style=\"padding: 10px; \"type=\"submit\" value=\"Проверить мои результаты\"></p>";
+		button.innerHTML = "<p><input type=\"submit\" value=\"Проверить мои результаты\"></p>";
+
 		document.body.appendChild(button);
+
